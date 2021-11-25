283222870be715fbe598eb774c91fbae506fea31
Kristiyan Bogdanov <kris@cyberstack.uk>
2021-11-25 15:14:15 +0200

    final refactoring

--- src/__tests__/Home.js
+++ src/__tests__/Home.js
@@ -6,17 +6,19 @@ import { format } from 'date-fns'
 afterEach(() => {
   mockAxios.reset();
 });
-beforeEach(() => {
-  mockAxios.reset();
-});
+
 test('renders the default home component', () => {
   render(<Home geo={{ city: 'Pleven' }} />);
   const linkElement = screen.getByText(/Check the weather for other locations/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('renders the no data view', () => {
+test('renders the no data view', async () => {
   render(<Home geo={{ city: 'Plev' }} />);
-  const linkElement = screen.getByText(/Please refine your search/i);
+  mockAxios.get.mockRejectedValue(new Error('Not Found'))
+  let linkElement = screen.getByText(/Loading .../i);
+  expect(linkElement).toBeInTheDocument();
+  await waitFor(() => expect(mockAxios.get).toHaveBeenCalledTimes(1))
+  linkElement = screen.getByText(/Please refine your search/i);
   expect(linkElement).toBeInTheDocument();
 });
 
@@ -44,7 +46,7 @@ test('expect weather API to throw exception', () => {
 test('renders the weather cards', async () => {
   mockAxios.get.mockResolvedValue({ data });
   render(<Home geo={{ city: 'Pleven' }} />);
-  let linkElement = screen.getByText(/Please refine your search/i);
+  let linkElement = screen.getByText(/Loading .../i);
   expect(linkElement).toBeInTheDocument();
   await waitFor(() => expect(mockAxios.get).toHaveBeenCalledTimes(1))
   linkElement = screen.getByText(format(new Date(data.list[0].dt_txt), 'cccc'))
@@ -53,7 +55,7 @@ test('renders the weather cards', async () => {
 
 test('should trigger the search', async () => {
   mockAxios.get.mockResolvedValue({ data });
-  render(<Home geo={{}} />);
+  render(<Home geo={null} />);
   let linkElement = screen.getByText(/Please refine your search/i);
   expect(linkElement).toBeInTheDocument();
   fireEvent.keyUp(document.querySelector('#search-box'), { target: { value: 'Pleven' } })
@@ -65,7 +67,7 @@ test('should trigger the search', async () => {
 test('renders the weather cards & opens the modal', async () => {
   mockAxios.get.mockResolvedValue({ data });
   render(<Home geo={{ city: 'Pleven' }} />);
-  let linkElement = screen.getByText(/Please refine your search/i);
+  let linkElement = screen.getByText(/Loading .../i);
   expect(linkElement).toBeInTheDocument();
   await waitFor(() => expect(mockAxios.get).toHaveBeenCalledTimes(1))
   linkElement = screen.getByText(format(new Date(data.list[0].dt_txt), 'cccc'))
